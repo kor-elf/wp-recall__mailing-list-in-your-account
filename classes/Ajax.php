768e4353a822a38a39e481ab7de17be016d15529
kor-elf <kor-elf@kor-elf.localdomain>
2020-02-05 00:04:20 +0600

    Метод getContactInGroups = готов.

--- classes/Ajax.php
+++ classes/Ajax.php
@@ -16,8 +16,33 @@ class Ajax
 
         $data = [];
 
+        $current_user = wp_get_current_user();
+        if (!empty($current_user)) {
+            // Ищим контакты у которых указана почта пользователя, что бы узнать его ID от сервиса
+            $contacts = ESputnik::getSearchContacts($current_user->user_email);
 
-        echo json_encode($data);        
+            if (!empty($contacts)) {
+
+                foreach ($contacts as $contactKey => $contactObject) {
+
+                    // Ищим контакт по ID, что бы узнать к каким группам пользователь подписан
+                    $contact = ESputnik::getContact($contactObject->id);
+                    if (!empty($contact) && !empty($contact->groups)) {
+
+                        foreach ($contact->groups as $groupKey => $groupObject) {
+                            $data[] = $groupObject->id;
+                        }
+                        
+                    }
+
+                }
+
+            }
+
+        }
+
+        echo json_encode($data);
+
         wp_die();
     }
 }
