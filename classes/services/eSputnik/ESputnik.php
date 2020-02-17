c92ac8d20d10fbc4b7ea5bfad332c1f17ab1ed15
kor-elf <kor-elf@kor-elf.localdomain>
2020-02-18 00:06:05 +0600

    Не рабочий вариант метод отписки от данного сервиса.

--- classes/services/eSputnik/ESputnik.php
+++ classes/services/eSputnik/ESputnik.php
@@ -48,18 +48,58 @@ class ESputnik extends BaseService implements ServiceInterface
         return $return;
     }
 
+    /**
+     * Отписываем контакт от группы
+     *
+     * @contactId = integer
+     * @groupId   = integer
+     *
+     * return boolean
+     */
+    public static function unsubscribe($contactId, $groupId)
+    {
 
+        /*
+        $json_contact_value = new \stdClass();
+
+        $contact = new \stdClass();
+        $contact->channels = array(
+        	array('type'=>'email', 'value' => 'i@kor-elf.net'),
+        );
+        $json_contact_value->contact = $contact;
+        $json_contact_value->groups = array();	// группы, в которые будет помещен контакт
+
+        $return = self::send('v1/contact/subscribe', $json_contact_value);
+
+        */
+
+
+/*
+        $return = self::send('v1/group/static/' . $groupId . '/contact/' . $contactId);
+
+        print_r($return);
+        exit;
+*/
+
+        $return = self::send('v1/contact/' . $contactId . '/subscriptions', ['subscriptions' => []]);
+
+        print_r($return);
+        exit;
+
+
+    }
 
 
 
     /**
      * Отправлять запросы к APi сервиса
      *
-     * @url = string
+     * @url  = string
+     * @post = array
      *
      * return object
      */
-    private static function send($url)
+    private static function send($url, $post = [])
     {
         $tokens = Tokens::getToken();
 
@@ -69,6 +109,13 @@ class ESputnik extends BaseService implements ServiceInterface
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
         curl_setopt($ch, CURLOPT_URL, 'https://esputnik.com/api/' . $url);
+
+        if (!empty($post)) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+        	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        }
+
         curl_setopt($ch, CURLOPT_USERPWD, $tokens);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSLVERSION, 6);
